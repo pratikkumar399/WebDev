1f1c1a488200a2eab63da295eff8450be00a4661
Pratik <pratikkumar399@gmail.com>
2022-06-30 12:50:10 +0530

    added style to components

--- tictactoe/src/components/Square.jsx
+++ tictactoe/src/components/Square.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-
 const Square = ({ value }) => {
     return (
-        <button type="button">{value}</button>
+        <button type="button" className="square">{value}</button>
     )
 }
 
