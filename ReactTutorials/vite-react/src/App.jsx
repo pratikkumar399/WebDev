2d87ee99db22672e67e859226d59db05a886c33c
raipratik <pratikkumar399@gmail.com>
2023-05-12 01:46:20 +0530

    making a todo app

--- ReactTutorials/vite-react/src/App.jsx
+++ ReactTutorials/vite-react/src/App.jsx
@@ -1,13 +1,32 @@
-import styles from './styles';
+
+import { useState } from "react";
 
 const App = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInput = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const addTasks = () => {
+    const newTasks = [...todoList, inputValue];
+    setTodoList(newTasks);
+  }
+
   return (
     <>
-      <h1 className={styles.style1}>Hello world!</h1>
-      <h1 className={styles.style2} >Hello world!</h1 >
-
+      <div className="text-center w-84">
+        <input onChange={handleInput} className="w-1/3 border-4 border-sky-800" type="text" />
+        <button onClick={addTasks}>Add Tasks</button>
+      </div>
+      <div>
+        {inputValue}
+      </div>
     </>
   )
 }
 
 export default App
+
+
