50c8745ffdc92be97d49de16d504455db34dff1f
raipratik <pratikkumar399@gmail.com>
2023-06-07 22:20:02 +0530

    learning fs module in nodejs

---
+++ NodeJs/imports_exports/readfile.js
@@ -0,0 +1,35 @@
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+
+// using the promises to avoid callback hell
+const fileOps = async () => {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, '', 'read.txt'), 'utf8');
+        console.log(data);
+        await fsPromises.writeFile(path.join(__dirname, '', 'write.txt'), 'Writing text');
+        await fsPromises.appendFile(path.join(__dirname, '', 'write.txt'), '\nAdding more text');
+        await fsPromises.rename(path.join(__dirname, '', 'write.txt'), path.join(__dirname, '', 'updatewrite.txt'));
+
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+fileOps();
+
+// fs.readFile(path.join(__dirname, '', 'read.txt'), 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// })
+
+// fs.readFile('read.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     else console.log(data);
+// })
+
+process.on('uncaughtException', err => {
+    console.log(err);
+    process.exit(1);
+})
