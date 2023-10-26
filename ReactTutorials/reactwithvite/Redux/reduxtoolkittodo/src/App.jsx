686815b23fa72182ebe29745c3d2a611f13e02fd
raipratik <pratikkumar399@gmail.com>
2023-10-26 23:13:11 +0530

    done with redux basics

---
+++ ReactTutorials/reactwithvite/Redux/reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './components/Addtodos'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn Redux</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
---
+++ ReactTutorials/reactwithvite/Redux/reduxtoolkittodo/src/app/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from '../feature/todo/todoSlice';
+
+export const store = configureStore({
+    reducer: todoReducer
+}); 
