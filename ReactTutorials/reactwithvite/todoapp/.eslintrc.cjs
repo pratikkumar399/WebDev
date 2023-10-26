97895bdb5709d69543a66b1d53fffdc478c7b371
raipratik <pratikkumar399@gmail.com>
2023-10-26 15:23:37 +0530

    done with the todo app using context app

--- ReactTutorials/reactwithvite/todoapp/.eslintrc.cjs
+++ ReactTutorials/reactwithvite/todoapp/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -16,5 +17,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': 'off',
+    'react/prop-types': 'off',
   },
 }
--- ReactTutorials/reactwithvite/todoapp/src/App.jsx
+++ ReactTutorials/reactwithvite/todoapp/src/App.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 import { TodoProvider } from './contexts';
 import './App.css'
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prevTodos) => [...prevTodos, { id: Date.now(), ...todo, completed: false }]);
+    setTodos((prevTodos) => [{ id: Date.now(), ...todo }, ...prevTodos]);
+    console.log("Item added")
   };
 
   const updateTodo = (id, todo) => {
@@ -18,9 +21,13 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {
+      ...prevTodo,
+      completed: !prevTodo.completed
+    } : prevTodo))
   }
 
+  // getting items from local storage
   useEffect(() => {
     const todos = JSON.parse(localStorage.getItem('todos'));
 
@@ -29,24 +36,32 @@ function App() {
     }
   }, []);
 
+  // setting items to local storage
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos));
   }, [todos]);
 
-
-
-
   return (
     <TodoProvider value={{ todos, toggleComplete, deleteTodo, updateTodo, addTodo }}>
-      <div className="bg-[#172842] min-h-screen py-8">
+      <div className="bg-[#172842] h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
             {/* Todo form goes here */}
+            <TodoForm />
 
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {
+              todos.map((todo) => (
+                // using unique id as key, this helps in better performance
+                <div key={todo.id} className='w-full'>
+                  <TodoItem todo={todo} />
+                </div>
+              )
+              )
+            }
           </div>
         </div>
       </div>
