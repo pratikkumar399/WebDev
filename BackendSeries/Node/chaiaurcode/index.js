ef31f82b569a98151df15a133e981440023bba8e
raipratik <pratikkumar399@gmail.com>
2023-11-14 02:04:21 +0530

    done for today

--- BackendSeries/Node/chaiaurcode/index.js
+++ BackendSeries/Node/chaiaurcode/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+const port = process.env.PORT || 3000
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 }
@@ -11,6 +14,6 @@ app.get("/github", (req, res) => {
     res.send("pratikkumar399");
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
