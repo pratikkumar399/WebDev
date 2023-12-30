1fa9634960353ee3613129b6455bafd8de2fe4a9
raipratik <pratikkumar399@gmail.com>
2023-12-30 19:55:43 +0530

    function parameter annotation in ts

---
+++ typescript/tuts/tuts1/funcParaAnnotation.ts
@@ -0,0 +1,16 @@
+// function parameter annotation in ts are used to specify the exwpected type of 
+// the parameter in a function
+
+// regular function
+
+function add(num1 : number , num2 : number){
+    return num1 + num2;
+}
+
+console.log(add(10, 20));
+
+
+// arrow function
+
+const addArrow = (num1 : number , num2 : number) => num1 + num2;
+console.log(addArrow(10, 20));
