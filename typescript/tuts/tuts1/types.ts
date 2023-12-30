2555be64ca2d0e41eacf24538fd5e236ac222225
raipratik <pratikkumar399@gmail.com>
2023-12-30 19:31:07 +0530

    type in ts

---
+++ typescript/tuts/tuts1/types.ts
@@ -0,0 +1,15 @@
+// strings
+
+let myName: string = "Pratik";
+console.log(myName, typeof myName);
+
+// number
+let age: number = 21;
+console.log(age, typeof age);
+
+
+let isStudent: boolean = true;
+isStudent = false;
+console.log(isStudent, typeof isStudent);
+
+
