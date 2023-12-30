1729c9d113c8c8761846fc527d64c32fcde39977
raipratik <pratikkumar399@gmail.com>
2023-12-30 19:39:07 +0530

    type inference in ts

---
+++ typescript/tuts/tuts1/typeInference.ts
@@ -0,0 +1,12 @@
+let userName = "Pratik";
+let tsHard = true;
+let yourAge = 21;
+
+
+console.log(userName);
+console.log(tsHard);
+console.log(yourAge);
+
+
+// error
+// userName = 10; due to type inference(typeScript will infer the type of variable based on the value assigned to it)
