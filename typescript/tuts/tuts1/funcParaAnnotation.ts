81caac1ada0cc0d33e8ceb2824485ff4b6696eb1
raipratik <pratikkumar399@gmail.com>
2024-01-05 22:48:22 +0530

    doing ts

--- typescript/tuts/tuts1/funcParaAnnotation.ts
+++ typescript/tuts/tuts1/funcParaAnnotation.ts
@@ -14,3 +14,6 @@ console.log(add(10, 20));
 
 const addArrow = (num1 : number , num2 : number) => num1 + num2;
 console.log(addArrow(10, 20));
+
+const double = (num : number) => num * 2;
+console.log(double(10));
