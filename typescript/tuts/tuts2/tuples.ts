ffd4aa394dce4473f0132c16ad9efdad8f837422
raipratik <pratikkumar399@gmail.com>
2024-01-08 17:41:10 +0530

    tuples in ts

---
+++ typescript/tuts/tuts2/tuples.ts
@@ -0,0 +1,9 @@
+// tuples in typescript
+
+// tuple is a collection of values of different types
+// tuples are fixed in size
+// tuples are ordered and like arrays, we can access the items using index
+let tuple1: [string, number] = ["hello", 10];
+
+// accessing the items of tuple using index
+console.log(tuple1[0]);
