be109a1e0c927a5af09f88b851a790ab8d62b24a
raipratik <pratikkumar399@gmail.com>
2024-01-09 01:30:29 +0530

    access modifiers in oops

--- typescript/tuts/oops/oops1.ts
+++ typescript/tuts/oops/oops1.ts
@@ -1,12 +1,41 @@
 class Person {
-    name : string;
-    age : number;
+    public name : string;
+    private age : number;
+    protected gender : string;
 
-    constructor(name : string , age :number){
+    constructor(name : string ,age : number ,  gender : string){
         this.name = name;
         this.age = age;
+        this.gender = gender;
+    }
+
+    public getName(): string{
+        return this.name;
+    }
+
+    public getAge(): number{
+        return this.age;
+    }
+
+    protected getGender(): string{
+        return this.gender;
     }
 }
 
-const person = new Person("Pratik", 21);
-console.log(person); // Person { name: 'Pratik', age: 21 }
+class Man extends Person {
+    constructor(name : string , age : number , gender : string){
+        super(name , age , gender)
+    }
+
+    public getInformation(): string{
+        return `${this.getName()} ${this.getAge()} ${this.getGender()}`;
+    }
+
+}
+
+
+// const person = new Person("Pratik", 21, "male");
+// console.log(person); 
+
+const ManOne = new Man("Person" , 21 , "male");
+console.log(ManOne.getInformation());
