7966bebad98cb6ef08398cc1a7f01882e3c61b38
raipratik <pratikkumar399@gmail.com>
2024-01-08 21:53:39 +0530

    enums in ts

---
+++ typescript/tuts/tuts2/enums.ts
@@ -0,0 +1,13 @@
+// enums is a way to define a set of named constants. 
+// enums allow you to define a collection of related values that can be used interchangeably.
+
+enum Direction {
+    East = 1,
+    West = 2,
+    North = 3,
+    South = 4
+}
+
+const currentDirection = Direction.East;
+
+console.log(currentDirection); // 0
