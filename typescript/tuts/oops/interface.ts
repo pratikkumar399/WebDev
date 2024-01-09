1ef36727100bdcf601136de12c9125750ff79d0a
raipratik <pratikkumar399@gmail.com>
2024-01-10 00:47:09 +0530

    done with interfaces

--- typescript/tuts/oops/interface.ts
+++ typescript/tuts/oops/interface.ts
@@ -27,11 +27,44 @@ interface add{
 // console.log(addFunction(10,20));
 
 const addFunction : add = (x , y) => x + y;
+const subFunction : add = (x , y) => x - y;
 
 console.log(addFunction(10,20));
+console.log(subFunction(10,20));
 
-function performAddition(operation: add, a: number, b: number): number {
-    return operation(a, b);
+
+
+// function performAddition(operation: add, a: number, b: number): number {
+//     return operation(a, b);
+// }
+
+// console.log(performAddition(addFunction, 10, 20));
+
+// INTERFACE FOR CLASSES
+
+interface animalPlanet{
+    name : string;
+    type : string;
+    total : number;
+    printInfo() : string;
 }
 
-console.log(performAddition(addFunction, 10, 20));
+class Dog implements animalPlanet{
+    name : string;
+    type : string;
+    total : number;
+
+    constructor(name : string , type : string , total : number){
+        this.name = name;
+        this.type = type;
+        this.total = total;
+    }
+
+    printInfo() : string{
+        return `${this.name} ${this.type} ${this.total}`;
+    }
+   
+}
+
+const dog = new Dog("Dog" , "Pet" , 100);
+console.log(dog.printInfo());
