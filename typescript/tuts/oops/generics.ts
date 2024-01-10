f90adb8e585e83826193e0b3544c2197a0066d20
raipratik <pratikkumar399@gmail.com>
2024-01-11 01:39:17 +0530

    done with generics

--- typescript/tuts/oops/generics.ts
+++ typescript/tuts/oops/generics.ts
@@ -18,3 +18,70 @@ function priintt<T>(a: T): T {
 console.log(priintt<number>(10));
 console.log(priintt<string>("Pratik"));
 console.log(priintt<boolean>(true)); 
+
+function identityType<T>(val : T) :T{
+    return val;
+}
+
+console.log(identityType<number>(10));
+console.log(identityType<string>("Pratik"));
+
+interface DOG{
+    name : string;
+    breed : string;
+}
+
+function uniqueIdentifier<T>(val : T , defualttype : T):[T,T]{
+    return [val , defualttype];
+}
+
+const dog1 = uniqueIdentifier<DOG>(
+    {name : "hatt",breed : "German Shepherd"},
+    {name : "bhakk",breed : "German Shepherd"}
+);
+
+console.log(dog1);
+
+
+function getRandomValue<t>(obj : {[key : string] : t}) : {
+    key : string , value : t
+}{
+    const keys = Object.keys(obj);
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    const randomKey = keys[randomIndex];
+    return {
+        key : randomKey,
+        value : obj[randomKey]
+    }
+}
+
+const stringg = {a : "Apple" , b : "Banana" , 2 : "Cherry" , d : "Dew" , e : "Egg"};
+const randomString = getRandomValue<string>(stringg);
+console.log(randomString);
+
+
+const fruits = ["Apple" , "Banana" , "Cherry" , "Dew" , "Egg"];
+const filteredFruit = fruits.filter((fruit) => fruit.length > 4);
+console.log(filteredFruit);
+
+class Box<T>{
+    value :T;
+
+    constructor(value : T){
+        this.value = value;
+    }
+
+    getValue() : T{
+        return this.value;
+    }
+
+    setValue(value : T) : void{
+        this.value = value;
+    }
+}
+
+const box = new Box<string>("Himanshi");
+console.log(box.getValue());
+
+const box1 = new Box<number>(100);
+console.log(box1.getValue());
