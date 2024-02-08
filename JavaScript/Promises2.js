f5ca2acfbf270459d50414043602d25d981b0f7a
raipratik <pratikkumar399@gmail.com>
2024-02-09 03:05:22 +0530

    practice questions

--- JavaScript/Promises2.js
+++ JavaScript/Promises2.js
@@ -1,29 +1,35 @@
 const promises = new Promise(function (resolve, reject) {
     setTimeout(function () {
-        resolve('Success');
-        console.log("Promise is resolved")
+        reject('Error');
+        // resolve('Success');
+        // console.log("Promise is resolved")
+        console.log("Error in promise")
     }, 1000);
 })
 
-promises.then(function (value) {
-    console.log(value);
-});
+promises
+    .then(function (value) {
+        console.log(value);
+    })
+    .catch(function (error) {
+        console.log("OOPs ", error);
+    });
 
 
-const promise1 = new Promise((resolve, reject) => {
-    resolve("Promise 1 Resolved");
-});
+// const promise1 = new Promise((resolve, reject) => {
+//     resolve("Promise 1 Resolved");
+// });
 
-const promise2 = new Promise((resolve, reject) => {
-    resolve("Promise 2 Resolved");
-});
+// const promise2 = new Promise((resolve, reject) => {
+//     resolve("Promise 2 Resolved");
+// });
 
-const promise3 = new Promise((resolve, reject) => {
-    resolve("Promise 3 Resolved");
-});
+// const promise3 = new Promise((resolve, reject) => {
+//     resolve("Promise 3 Resolved");
+// });
 
-Promise.all([promise1, promise2, promise3]).then((values) => {
-    console.log(values);
-}).catch((error) => {
-    console.log(error);
-});
+// Promise.all([promise1, promise2, promise3]).then((values) => {
+//     console.log(values);
+// }).catch((error) => {
+//     console.log(error);
+// });
--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -4,10 +4,110 @@ const array2 = array.map((value, index) => {
     return value * 2;
 })
 
-const array3 = array.forEach((value, index) => {
+array.forEach((value, index) => {
     value = value * 2;
-    return value;
+    // console.log(value);
 })
 
-console.log(array2);
-console.log(array3);
+// console.log(array2);
+
+
+// const pr = new Promise((resolve, reject) => {
+//     const intervalId = setInterval(() => {
+//         console.log("This runs every 1 second");
+//     }, 1000); // Interval runs every 1 second
+
+//     // Stop the setInterval after 10 seconds
+//     setTimeout(() => {
+//         clearInterval(intervalId);
+//         resolve(); // Resolve the promise after stopping the interval
+//     }, 10000); // 10 seconds
+// });
+
+// pr.then(() => {
+//     console.log("Promise is resolved");
+// }).catch((error) => {
+//     console.log(error);
+// });
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts").then(response => {
+//     if (response.ok) {
+//         return response.json();
+//     } else {
+//         throw new Error("Something went wrong");
+//     }
+// }).then(data => {
+//     console.log(data[0]);
+// }).catch(error => {
+//     console.log(error);
+// });
+
+// lets do that with async await
+async function fetchData() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await response.json();
+        console.log(data[0]);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+fetchData();
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(arr.map((val) => val * 2));
+// console.log(arr.filter((val) => val > 5));
+
+let max = 0;
+
+arr.forEach((val) => {
+    max = Math.max(val, max);
+})
+
+console.log(max);
+
+
+function a() {
+    b();
+    function b() {
+        console.log(c);
+    }
+}
+
+var c = 10;
+a();
+
+
+// promise chaining
+
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Promise 1 Resolved");
+    }, 1000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Promise 2 Resolved");
+    }, 2000);
+});
+
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Promise 3 Resolved");
+    }, 3000);
+});
+
+promise1.then(() => {
+    return promise2;
+}).then(() => {
+    return promise3;
+}).then(() => {
+    console.log("All promises resolved");
+}).catch((error) => {
+    console.log(error);
+});
